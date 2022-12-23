44f57a3aafc6db69b479514a659a8d104b2a115b
Alex Van Bussum <avanbussum@gmail.com>
2022-12-22 20:46:40 -0700

    round 2

--- components/AuthorList.tsx
+++ components/AuthorList.tsx
@@ -13,7 +13,6 @@ function AuthorList({authors}: Props) {
             <h1 className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 mb-6 text-5xl font-serif">The Author</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-x-10 gap-y-16 pb-24">
                 {/* Posts */}
-                {/**Test chajd */}
 
                 {authors.map(author => (
                     <ClientSideRoute key={author._id} route={`/author/${author.slug.current}`}>
