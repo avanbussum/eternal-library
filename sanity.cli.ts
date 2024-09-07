a24bd1e65f32d4eb40a6994b2e4d2a99bf80405c
Alex Van Bussum <avanbuss@Alexs-MacBook-Pro.local>
2024-09-07 14:08:37 -0500

    removed env hardcodes

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,7 @@
 import {defineCliConfig} from 'sanity/cli'
 
-// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-const projectId = 'o3e3qd6t';
-const dataset = 'production';
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineCliConfig({
   api: {
--- sanity.config.ts
+++ sanity.config.ts
@@ -7,10 +7,8 @@ import StudioNavbar from './components/StudioNavbar';
 import Logo from'./components/Logo';
 import { getDefaultDocumentNode } from './structure';
 
-// const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
-const projectId = 'o3e3qd6t';
-const dataset = 'production';
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
   basePath: "/studio",
